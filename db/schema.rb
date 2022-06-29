f4f887adb89d4cac5a2da49f68ec0a0a4ad89f8b
Dima Chavro <dchavro@gmail.com>
2022-06-29 15:41:51 +0100

    Add title to Posts and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_25_120732) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_29_144017) do
   create_table "posts", force: :cascade do |t|
     t.text "post"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.text "title"
   end
 
   create_table "users", force: :cascade do |t|
