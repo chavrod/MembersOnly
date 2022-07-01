6fc61ca6a2cca6719551c967b0b6c62a5e353ebd
Dima Chavro <dchavro@gmail.com>
2022-07-01 10:22:15 +0100

    Modify MVC to show articles creation & update times

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.order(created_at: :desc)
   end
 
   # GET /posts/1 or /posts/1.json
@@ -66,6 +66,6 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:post)
+      params.require(:post).permit(:post, :title)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,25 @@
 class Post < ApplicationRecord
+  include ActionView::Helpers::DateHelper
+
   belongs_to :user
+
+  def unedited?
+    created_at == updated_at
+  end
+
+  def unedited_history
+    if created_at > 4.days.ago
+      time_ago_in_words(created_at) + ' ago'
+    else
+      created_at.strftime('%b %-d %Y')
+    end
+  end
+
+  def edited_history
+    if updated_at > 4.days.ago
+      time_ago_in_words(updated_at) + ' ago'
+    else
+      updated_at.strftime('%b %-d %Y')
+    end
+  end
 end
